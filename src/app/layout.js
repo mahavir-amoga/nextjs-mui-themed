4da0a6b718eafe20277e2dbe17c4477702571517
mahavir <mahavirsinh.chauhan@amoga.io>
2024-11-20 10:56:27 +0530

    package updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,11 @@
 "use client";
 // app/layout.js
 import { CacheProvider } from "@emotion/react";
-import { CssBaseline, ThemeProvider } from "@mui/material";
+import { CssBaseline, Grid2, ThemeProvider } from "@mui/material";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 import createEmotionCache from "./createEmotionCache";
+import "./globals.css";
 import theme from "./theme";
-import "./globals.css"
 
 // Create a client-side cache for Emotion
 const clientSideEmotionCache = createEmotionCache();
@@ -18,7 +18,19 @@ export default function RootLayout({ children }) {
           <CacheProvider value={clientSideEmotionCache}>
             <ThemeProvider theme={theme}>
               <CssBaseline />
-              {children}
+              <Grid2
+                display="flex"
+                flexDirection="column"
+                justifyContent="space-between"
+              >
+                <Grid2 sx={{ width: "100%", border: "2px solid red" }}>
+                  Header
+                </Grid2>
+                <Grid2>{children}</Grid2>
+                <Grid2 sx={{ width: "100%", border: "2px solid red" }}>
+                  Footer
+                </Grid2>
+              </Grid2>
             </ThemeProvider>
           </CacheProvider>
         </AppRouterCacheProvider>
